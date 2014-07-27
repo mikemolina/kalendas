9773c0d41351259ccefb1045058aa2340722bbd1
Mike Molina <mmolina.unphysics@gmail.com>
2014-07-27 13:04:16 -0500

    Added function to maintain po files

--- autoclean.sh
+++ autoclean.sh
@@ -2,6 +2,18 @@
 #
 # autoclean.sh - Shellscript para limpiar los archivos 'autogenerados'.
 
+# Funcion para mantener fecha de ultima actualizacion de archivos po
+podowndate() {
+    cp $1 tmp
+    rm $1
+    # Fecha ultima actualizacion archivos po
+    tdown='2014-07-19 21:22-0500'
+    tpot=`cat tmp | grep 'POT-Creation-Date:' | sed -e s/\"//g | sed -e 's/.*: \(.*\)[\]n/\1/'`
+    tpo=`cat tmp | grep 'PO-Revision-Date:' | sed -e s/\"//g | sed -e s'/.*: \(.*\)[\]n/\1/'`
+    sed -e "s/${tpo}/${tdown}/g" -e "s/${tpot}/${tdown}/g" tmp > $1
+    rm tmp
+}
+
 test ! -f Makefile || make maintainer-clean
 
 # Generados por autoconf
@@ -10,6 +22,10 @@ rm -fR build-aux
 rm -fR m4
 if [ -d po ]; then
    cp -f po/*.po .
+   for lang in `ls *.po`
+   do
+      podowndate $lang; 
+   done
 fi
 rm -fR po
 rm -f configure
